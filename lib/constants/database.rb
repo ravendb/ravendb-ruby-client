d762565902155af2e4eddd5f390ecf4cea4209f9
Alex Serdukov <a.serdukov@mobilunity.com>
2017-08-04 17:09:51 +0300

    - added gemspec - added constants

---
+++ lib/constants/database.rb
@@ -0,0 +1,52 @@
+class AccessMode
+  None = "None"
+  ReadOnly = "ReadOnly"
+  ReadWrite = "ReadWrite"
+  Admin = "Admin"
+end
+
+class ConcurrencyCheckMode
+  Auto = "Auto"
+  Forced = "Forced"
+  Disabled = "Disabled"
+end
+
+class FieldIndexingOption
+  No = "No"
+  Analyzed = "Analyzed"
+  NotAnalyzed = "NotAnalyzed"
+  Default = "Default"
+end
+
+class FieldTermVectorOption
+  No = "No"
+  Yes = "Yes"
+  WithPositions = "WithPositions"
+  WithOffsets = "WithOffsets"
+  WithPositionsAndOffsets = "WithPositionsAndOffsets"
+end
+
+class IndexLockMode
+  Unlock = "Unlock"
+  LockedIgnore = "LockedIgnore"
+  LockedError = "LockedError"
+  SideBySide = "SideBySide"
+end
+
+class IndexPriority
+  Low = "Low"
+  Normal = "Normal"
+  High = "High"
+end
+
+class SortOptions
+  None = "None"
+  Str = "String"
+  Numeric = "Numeric"
+end  
+
+class OperationStatus
+  Completed = "Completed"
+  Faulted = "Faulted"
+  Running = "Running"
+end
---
+++ ravendb-nodejs-client.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'ravendb'
+  s.version     = '4.0.0-beta'
+  s.date        = '2017-08-04'
+  s.summary     = "RavenDB"
+  s.description = "RavenDB client for Ruby"
+  s.authors     = ["Hibernating Rhinos"]
+  s.email       = 'support@ravendb.net'
+  s.files       = ["lib/ravendb.rb"]
+  s.homepage    =
+    'http://ravendb.net'
+  s.license       = 'MIT'
+end
