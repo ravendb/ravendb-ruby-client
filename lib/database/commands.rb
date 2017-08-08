4b20e1ef3bc454c2a4af21f5e7f3085fc18b01a1
Alex Serdukov <a.serdukov@mobilunity.com>
2017-08-08 19:54:09 +0300

    - batch command

--- lib/database/commands.rb
+++ lib/database/commands.rb
@@ -111,6 +111,37 @@ module RavenDB
     end  
   end  
 
+  class BatchCommand < RavenCommand
+    @commands_array = []
+
+    def initialize(commands_array)
+      super('', Net::HTTP::Post::METHOD)
+      @commands_array = commands_array
+    end
+
+    def create_request(server_node)
+      commands = @commands_array
+      assert_node(server_node)
+
+      if !commands.all? { |data| data && data.is_a?(RavenCommandData) }
+        raise InvalidOperationException, 'Not a valid command'
+      end
+
+      @end_point = "#{server_node.url}/databases/#{server_node.database}/bulk_docs"
+      @payload = {"Commands" => commands.map({ |data| data.to_json })}
+    end
+
+    def set_response(response)
+      result = super(response)
+
+      if !response.body {
+        raise InvalidOperationException, 'Invalid response body received'
+      }
+
+      return result["Results"]
+    end
+  end
+
   class RavenCommandData
     @type = nil
     @id = nil
