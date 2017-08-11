da4916d32f98ac9673901efd45edf01f069e62c8
Alex Serdukov <a.serdukov@mobilunity.com>
2017-08-11 18:57:27 +0300

    - finished request executor

--- lib/requests/request_executor.rb
+++ lib/requests/request_executor.rb
@@ -24,7 +24,7 @@ class RequestExecutor
   @_await_first_topology_lock = nil
   @_update_topology_lock = nil
   @_update_failed_node_timer_lock = nil
-  @_first_topology_update_thread = nil
+  @_first_topology_update = nil
 
   def initial_database
     @_initialDatabase
@@ -70,6 +70,53 @@ class RequestExecutor
     end
   end
 
+  def execute(command)
+    chosen_node = nil
+    chosen_node_index = -1
+
+    await_first_topology_update
+    selector = @_nodeSelector;
+    chosen_node = selector.current_node;
+    chosen_node_index = selector.current_node_index;
+
+    begin
+      response = execute_command(command, chosen_node)
+    rescue TopologyNodeDownException
+      response = handle_server_down(command, chosen_node, chosen_node_index)
+    end
+
+    return response
+  end  
+
+  protected
+  def await_first_topology_update()
+    is_fulfilled = false
+    first_topology_update = @_first_topology_update
+
+    if @_without_topology
+      return
+    end
+
+    @_await_first_topology_lock.syncronize do
+      if first_topology_update.equal?(@_first_topology_update)
+        is_fulfilled = true === first_topology_update;
+
+        if false === first_topology_update
+          start_first_topology_update(@_last_known_urls)
+        end  
+      end
+    end  
+
+    if !is_fulfilled
+      if is_first_topology_update_tries_expired
+        raise DatabaseLoadFailureException, 'Max topology update tries reached'
+      elsif
+        sleep .1
+        await_first_topology_update
+      end    
+    end      
+  end
+
   protected
   def prepare_command(command, server_node)
     command.create_request(server_node)
@@ -97,8 +144,13 @@ class RequestExecutor
     end
 
     request = prepare_command(command, node)
-    #TODO: handle server down & raise TopologyNodeDownException
-    response = Net::HTTP.request(request)
+    
+    begin
+      response = Net::HTTP.request(request)
+    rescue
+      raise TopologyNodeDownException, "Node #{server_node.url} is down"
+    end    
+
     is_server_error = [Net::HTTPRequestTimeout, Net::HTTPBadGateway,
       Net:HTTPGatewayTimeout, Net::HTTPServiceUnavailable].any? { 
       | response_type | response.is_a?(response_type)
@@ -138,15 +190,19 @@ class RequestExecutor
 
     @_last_known_urls = urls  
     @_first_topology_updates_tries++
-    @_first_topology_update_thread = Thread.new do
+    @_first_topology_update = Thread.new do
       for url in urls do
         update_topology(ServerNode.new(url, @_initial_database))
-        @_first_topology_update_thread = nil
+        @_first_topology_update = true
         break
       rescue
         next
       end  
+
+      @_first_topology_update = false 
     end    
+
+    @_first_topology_update.join
   end
 
   protected
@@ -187,6 +243,73 @@ class RequestExecutor
   def get_update_topology_command_class
     return GetTopologyCommand.class
   end  
+
+  protected
+  def handle_server_down(command, failed_node, failed_node_index)
+    next_node = nil
+    node_selector = @_node_selector
+    REQUEST_FAILED  = RavenServerEvent::REQUEST_FAILED
+
+    command.add_failed_node(failed_node)
+
+    @_update_failed_node_timer_lock.syncronize do
+      status = NodeStatus.new(failed_node_index, failed_node, {
+        |status| check_node_status(status)
+      })
+
+      @_failed_nodes_statuses[failed_node] = status
+      status.start_update
+    end  
+
+    emit(REQUEST_FAILED, failed_node)
+    next_node = node_selector.current_node
+
+    if !next_node || command.was_failed_with_node(next_node)
+      raise AllTopologyNodesDownException, "Tried all nodes in the cluster "\
+        "but failed getting a response"
+    end  
+
+    return execute_command(command, next_node)
+  end  
+
+  protected 
+  def check_node_status(node_status)
+    nodes = @_node_selector.nodes
+    index = node_status.node_index
+    node = node_status.node
+
+    if (index < nodes.size) && (node === nodes[index])
+      perform_health_check(node)
+    end
+  end
+
+  protected perform_health_check(server_node)
+    status = nil
+    is_still_failed = nil
+    command = GetStatisticsCommand.new(true)
+    NODE_STATUS_UPDATED = RavenServerEvent::NODE_STATUS_UPDATED    
+        
+    begin
+      response = Net::HTTP.request(prepare_command(command, server_node))
+      is_still_failed = response.is_a?(Net::HTTPOK)
+
+      if is_still_failed == true
+        emit(NODE_STATUS_UPDATED, server_node)
+
+        if @_failed_nodes_statuses.key?(server_node)
+          @_failed_nodes_statuses[server_node].dispose
+
+        end  
+      end  
+    rescue
+      is_still_failed = false
+    end    
+
+    if isStillFailed && @_failed_nodes_statuses.key?(server_node)
+      @_failed_nodes_statuses[server_node].retry_update()
+      @_failed_nodes_statuses.delete(server_node)
+    end
+  end
 
   protected
   def cancel_failing_nodes_timers
