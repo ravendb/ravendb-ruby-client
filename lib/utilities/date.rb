7653b03fe9d010118e30ff5a646f109fa72297e7
Alex Serdukov <a.serdukov@mobilunity.com>
2017-10-27 19:53:10 +0300

    - HiLo generators - added unit tests for HiLo generator

--- lib/utilities/date.rb
+++ lib/utilities/date.rb
@@ -3,13 +3,27 @@ require "database/exceptions"
 
 module RavenDB
   class DateUtil
+    DATE_FORMAT = "%Y%m%dT%H%M%S%6N0"
+
     def self.stringify(datetime)
       invalid_date_message = 'Invalid parameter passed to RavenDB'\
         '::DateUtil::stringify. It should be instance of DateTime'
 
       raise InvalidOperationException, invalid_date_message unless datetime.is_a?(DateTime)
 
-      return datetime.strftime("%Y%m%dT%H%M%S%6N0")
+      datetime.strftime(DATE_FORMAT)
+    end
+
+    def self.parse(datestring)
+      if datestring.end_with?("Z")
+        datestring = datestring.chomp("Z")
+      end
+
+      DateTime.strptime(datestring, DATE_FORMAT)
+    end
+
+    def self.zero_date
+      DateTime.new(1, 1, 1, 0, 0, 0)
     end
   end
 end
