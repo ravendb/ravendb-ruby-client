308e68123034cd0ce672d3a0420dce2805f407e1
Alex Serdukov <a.serdukov@mobilunity.com>
2017-10-30 17:51:37 +0200

    - JSON serializer tests: testing hashes, arrays and nested hashes / arrays

--- test/session_tests/document_serializing_test.rb
+++ test/session_tests/document_serializing_test.rb
@@ -18,7 +18,7 @@ class DocumentSerializingTest < TestBase
         "number_prop" => 2,
         "number_float_prop" => 2.5,
         "boolean_prop" => true,
-        "nul_prop" => nil,
+        "nil_prop" => nil,
         "array_prop" => [1, 2, 3]
       },
       "array_prop" => [1, 2, 3],
@@ -63,19 +63,96 @@ class DocumentSerializingTest < TestBase
       "deep_foo_prop" => Foo.name,
       "deep_array_foo_prop" => Foo.name
     }
+
+    @document = SerializingTest.new
+    RavenDB::JsonSerializer::from_json(@document, @json, {}, @nested_object_types)
   end
 
   def test_should_parse_scalars
-    document = SerializingTest.new
-    RavenDB::JsonSerializer::from_json(document, @json, {}, @nested_object_types)
+    assert(@document.string_prop.is_a?(String))
+    assert_equal(@document.string_prop, 'string')
+    assert(@document.number_prop.is_a?(Numeric))
+    assert_equal(@document.number_prop, 2)
+    assert(@document.number_float_prop.is_a?(Numeric))
+    assert_equal(@document.number_float_prop, 2.5)
+    assert_equal(@document.boolean_prop, true)
+    assert(@document.nil_prop.nil?)
+  end
 
-    assert(document.string_prop.is_a?(String))
-    assert_equal(document.string_prop, 'string')
-    assert(document.number_prop.is_a?(Numeric))
-    assert_equal(document.number_prop, 2)
-    assert(document.number_float_prop.is_a?(Numeric))
-    assert_equal(document.number_float_prop, 2.5)
-    assert_equal(document.boolean_prop, true)
-    assert(document.nil_prop.nil?)
+  def test_should_parse_arrays
+    assert(@document.array_prop.is_a?(Array))
+    assert_equal(@document.array_prop.size, 3)
+    assert_equal(@document.array_prop, [1, 2, 3])
+  end
+
+  def test_should_parse_deep_arrays
+    deep = @document.deep_array_prop[2]
+
+    assert(@document.deep_array_prop.is_a?(Array))
+    assert_equal(@document.deep_array_prop.size, 3)
+    assert_equal(@document.deep_array_prop, [1, 2, [3, 4]])
+
+    assert(deep.is_a?(Array))
+    assert_equal(deep.size, 2)
+    assert_equal(deep, [3, 4])
+  end
+
+  def test_should_parse_hashes
+    assert(@document.hash_prop.is_a?(Hash))
+    assert(@document.hash_prop.key?("string_prop"))
+    assert(@document.hash_prop.key?("number_prop"))
+    assert(@document.hash_prop.key?("number_float_prop"))
+    assert(@document.hash_prop.key?("boolean_prop"))
+    assert(@document.hash_prop.key?("nil_prop"))
+    assert(@document.hash_prop.key?("array_prop"))
+
+    assert(@document.hash_prop["string_prop"].is_a?(String))
+    assert_equal(@document.hash_prop["string_prop"], 'string')
+    assert(@document.hash_prop["number_prop"].is_a?(Numeric))
+    assert_equal(@document.hash_prop["number_prop"], 2)
+    assert(@document.hash_prop["number_float_prop"].is_a?(Numeric))
+    assert_equal(@document.hash_prop["number_float_prop"], 2.5)
+    assert_equal(@document.hash_prop["boolean_prop"], true)
+    assert(@document.hash_prop["nil_prop"].nil?)
+
+    assert(@document.hash_prop["array_prop"].is_a?(Array))
+    assert_equal(@document.hash_prop["array_prop"].size, 3)
+    assert_equal(@document.hash_prop["array_prop"], [1, 2, 3])
+  end
+
+  def test_should_parse_deep_hashes
+    deep = @document.deep_hash_prop["some_hash"]
+
+    assert(@document.deep_hash_prop.is_a?(Hash))
+    assert(@document.deep_hash_prop.key?('some_prop'))
+    assert_equal(@document.deep_hash_prop['some_prop'], 'someValue')
+
+    assert(deep.is_a?(Hash))
+    assert(deep.key?('some_prop'))
+    assert(deep['some_prop'], 'someValue')
+  end
+
+  def test_should_parse_mixed_deep_arrays_hashes
+    deep_hash = @document.deep_array_hash_prop[2]
+    deep_array_in_hash = deep_hash["some_array"]
+    deep_array = @document.deep_array_hash_prop[4]
+    deep_hash_in_array = deep_array[2]
+
+    assert(deep_hash.is_a?(Hash))
+    assert(deep_hash.key?('some_prop'))
+    assert_equal(deep_hash['some_prop'], 'someValue')
+
+    assert(deep_array_in_hash.is_a?(Array))
+    assert_equal(deep_array_in_hash.size, 2)
+    assert_equal(deep_array_in_hash, [3, 4])
+
+    assert(deep_array.is_a?(Array))
+    assert_equal(deep_array.size, 3)
+    assert_equal(deep_array[0], 7)
+    assert_equal(deep_array[1], 8)
+
+    assert(deep_hash_in_array.is_a?(Hash))
+    assert(deep_hash_in_array.key?('some_prop'))
+    assert_equal(deep_hash_in_array['some_prop'], 'someValue')
   end
 end
