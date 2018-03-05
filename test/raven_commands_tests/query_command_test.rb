cd0a7cad3b9b75963d645299688433b47fb3a182
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-05 22:48:32 +0100

    rubocop: Layout/ExtraSpacing

--- test/raven_commands_tests/query_command_test.rb
+++ test/raven_commands_tests/query_command_test.rb
@@ -42,7 +42,7 @@ class QueryCommandTest < RavenDatabaseIndexesTest
 
   def test_should_fail_with_no_existing_index
     assert_raises(RavenDB::RavenException) do
-      @_index_query = RavenDB::IndexQuery.new("from index 'IndexIsNotExists' WHERE Tag = 'Products'",  {}, nil, nil, wait_for_non_stale_results: true)
+      @_index_query = RavenDB::IndexQuery.new("from index 'IndexIsNotExists' WHERE Tag = 'Products'", {}, nil, nil, wait_for_non_stale_results: true)
       @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
     end
   end
