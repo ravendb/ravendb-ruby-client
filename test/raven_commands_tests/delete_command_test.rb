f9fd2a6ccc7be6c6296a47fc6cf6ca3e1169276c
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-11 17:38:34 +0100

    delete_command_test: prepare for conversion

--- test/raven_commands_tests/delete_command_test.rb
+++ test/raven_commands_tests/delete_command_test.rb
@@ -3,27 +3,36 @@ require "securerandom"
 require "minitest/autorun"
 require "spec_helper"
 
-class DeleteCommandTest < RavenDatabaseTest
+describe RavenDB::DeleteDocumentCommand do
   @_change_vector = nil
   @_other_change_vector = nil
 
   def setup
-    super()
+    @__test = RavenDatabaseTest.new(nil)
+    @__test.setup
 
-    @_request_executor.execute(RavenDB::PutDocumentCommand.new("Products/101", "Name" => "test", "@metadata" => {}))
-    response = @_request_executor.execute(RavenDB::GetDocumentCommand.new("Products/101"))
+    request_executor.execute(RavenDB::PutDocumentCommand.new("Products/101", "Name" => "test", "@metadata" => {}))
+    response = request_executor.execute(RavenDB::GetDocumentCommand.new("Products/101"))
     @_change_vector = response["Results"].first["@metadata"]["@change-vector"]
 
-    @_request_executor.execute(RavenDB::PutDocumentCommand.new("Products/102", "Name" => "test", "@metadata" => {}))
-    response = @_request_executor.execute(RavenDB::GetDocumentCommand.new("Products/102"))
+    request_executor.execute(RavenDB::PutDocumentCommand.new("Products/102", "Name" => "test", "@metadata" => {}))
+    response = request_executor.execute(RavenDB::GetDocumentCommand.new("Products/102"))
     @_other_change_vector = response["Results"].first["@metadata"]["@change-vector"]
+  end
+
+  def teardown
+    @__test.teardown
+  end
+
+  def request_executor
+    @__test.request_executor
   end
 
   def test_should_delete_with_no_change_vector
     command = RavenDB::DeleteDocumentCommand.new("Products/101")
 
     refute_raises(RavenDB::RavenException) do
-      @_request_executor.execute(command)
+      request_executor.execute(command)
     end
   end
 
@@ -31,13 +40,13 @@ class DeleteCommandTest < RavenDatabaseTest
     command = RavenDB::DeleteDocumentCommand.new("Products/102", @_other_change_vector)
 
     refute_raises(RavenDB::RavenException) do
-      @_request_executor.execute(command)
+      request_executor.execute(command)
     end
   end
 
   def test_should_fail_delete_if_change_vector_mismatches
     assert_raises(RavenDB::RavenException) do
-      @_request_executor.execute(RavenDB::DeleteDocumentCommand.new("Products/101", "#{@_change_vector}:BROKEN:VECTOR"))
+      request_executor.execute(RavenDB::DeleteDocumentCommand.new("Products/101", "#{@_change_vector}:BROKEN:VECTOR"))
     end
   end
 end
