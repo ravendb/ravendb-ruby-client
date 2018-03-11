0c540846fdc35c6233ccfcf3dfa9ee9507ca239d
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-11 18:16:00 +0100

    document_conversion_test: convert to rspec

--- spec/session_tests/document_conversion_spec.rb
+++ spec/session_tests/document_conversion_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 describe RavenDB::DocumentConventions do
   NOW = DateTime.now
 
-  def setup
+  before do
     @__test = RavenDatabaseTest.new(nil)
     @__test.setup
 
@@ -16,15 +16,15 @@ describe RavenDB::DocumentConventions do
     end
   end
 
-  def teardown
+  after do
     @__test.teardown
   end
 
-  def store
+  let(:store) do
     @__test.store
   end
 
-  def test_should_convert_on_load
+  it "converts on load" do
     id = "TestConversions/1"
 
     store.open_session do |session|
@@ -33,7 +33,7 @@ describe RavenDB::DocumentConventions do
     end
   end
 
-  def test_should_convert_on_store_then_reload
+  it "converts on store then reload" do
     id = "TestConversions/New"
 
     store.open_session do |session|
@@ -47,21 +47,20 @@ describe RavenDB::DocumentConventions do
     end
   end
 
-  def test_should_convert_on_query
+  it "converts on query" do
     store.open_session do |session|
-      results = session.query(
-        collection: "TestConversions"
-      )
-                       .where_greater_than("date", NOW)
-                       .wait_for_non_stale_results
-                       .all
+      results = session
+                .query(collection: "TestConversions")
+                .where_greater_than("date", NOW)
+                .wait_for_non_stale_results
+                .all
 
-      assert_equal(1, results.size)
+      expect(results.size).to(eq(1))
       check_doc("TestConversions/2", results.first)
     end
   end
 
-  def test_should_support_custom_id_property
+  it "supports custom id property" do
     id = nil
 
     store.conventions.add_id_property_resolver do |document_info|
@@ -81,12 +80,12 @@ describe RavenDB::DocumentConventions do
     store.open_session do |session|
       doc = session.load(id)
 
-      assert_equal(id, doc.item_id)
-      assert_equal("New Item", doc.item_title)
+      expect(doc.item_id).to(eq(id))
+      expect(doc.item_title).to(eq("New Item"))
     end
   end
 
-  def test_should_support_custom_serializer
+  it "supports custom serializer" do
     id = nil
 
     store.conventions.add_id_property_resolver do |document_info|
@@ -108,16 +107,16 @@ describe RavenDB::DocumentConventions do
     store.open_session do |session|
       doc = session.load(id)
 
-      assert_equal(doc.item_id, id)
-      assert_equal(doc.item_title, "New Item")
-      assert_equal(doc.item_options, [1, 2, 3])
+      expect(id).to(eq(doc.item_id))
+      expect(doc.item_title).to(eq("New Item"))
+      expect(doc.item_options).to(eq([1, 2, 3]))
 
       raw_entities_and_metadata = session.instance_variable_get("@raw_entities_and_metadata")
       info = raw_entities_and_metadata[doc]
       raw_entity = info[:original_value]
 
-      assert_equal("New Item", raw_entity["itemTitle"])
-      assert_equal("1,2,3", raw_entity["itemOptions"])
+      expect(raw_entity["itemTitle"]).to(eq("New Item"))
+      expect(raw_entity["itemOptions"]).to(eq("1,2,3"))
     end
   end
 
@@ -132,19 +131,19 @@ describe RavenDB::DocumentConventions do
   end
 
   def check_foo(foo, id_of_foo = 1)
-    assert(foo.is_a?(Foo))
-    assert_equal("Foos/#{id_of_foo}", foo.id)
-    assert_equal("Foo ##{id_of_foo}", foo.name)
-    assert_equal(id_of_foo, foo.order)
+    expect(foo.is_a?(Foo)).to(eq(true))
+    expect(foo.id).to(eq("Foos/#{id_of_foo}"))
+    expect(foo.name).to(eq("Foo ##{id_of_foo}"))
+    expect(foo.order).to(eq(id_of_foo))
   end
 
   def check_doc(id, doc)
-    assert(doc.is_a?(TestConversion))
-    assert_equal(id, doc.id)
-    assert(doc.date.is_a?(DateTime))
-    assert(doc.foos.is_a?(Array))
+    expect(doc.is_a?(TestConversion)).to(eq(true))
+    expect(doc.id).to(eq(id))
+    expect(doc.date.is_a?(DateTime)).to(eq(true))
+    expect(doc.foos.is_a?(Array)).to(eq(true))
 
     check_foo(doc.foo)
-    doc.foos.each_index { |index| check_foo(doc.foos[index], index + 2) }
+    doc.foos.each_index { |index| check_foo(doc.foos[index], (index + 2)) }
   end
 end
