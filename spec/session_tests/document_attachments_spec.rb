9e9900e61c69a64d2c1c3279c952e6e1fed577b6
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 13:10:47 +0100

    document_attachments_test: convert to rspec

--- spec/session_tests/document_attachments_spec.rb
+++ spec/session_tests/document_attachments_spec.rb
@@ -4,34 +4,34 @@ require "spec_helper"
 describe RavenDB::AttachmentOperation do
   ATTACHMENT = "47494638396101000100800000000000ffffff21f90401000000002c000000000100010000020144003b".freeze
 
-  def setup
+  before do
     @__test = RavenDatabaseTest.new(nil)
     @__test.setup
   end
 
-  def teardown
+  after do
     @__test.teardown
   end
 
-  def store
+  let(:store) do
     @__test.store
   end
 
-  def test_should_put_attachment
+  it "puts attachment" do
     store.open_session do |session|
       product = Product.new(nil, "Test Product", 10, "a")
 
       session.store(product)
       session.save_changes
 
-      refute_raises do
+      expect do
         store.operations.send(
           RavenDB::PutAttachmentOperation.new(product.id, "1x1.gif", [ATTACHMENT].pack("H*"), "image/gif"))
-      end
+      end.not_to(raise_error)
     end
   end
 
-  def test_should_get_attachment
+  it "gets attachment" do
     store.open_session do |session|
       attachment_raw = [ATTACHMENT].pack("H*")
       product = Product.new(nil, "Test Product", 10, "a")
@@ -45,15 +45,15 @@ describe RavenDB::AttachmentOperation do
       attachment_result = store.operations.send(
         RavenDB::GetAttachmentOperation.new(product.id, "1x1.gif", RavenDB::AttachmentType::Document))
 
-      assert_equal(attachment_raw, attachment_result[:stream])
-      assert_equal(product.id, attachment_result[:attachment_details][:document_id])
-      assert_equal("image/gif", attachment_result[:attachment_details][:content_type])
-      assert_equal("1x1.gif", attachment_result[:attachment_details][:name])
-      assert_equal(attachment_raw.size, attachment_result[:attachment_details][:size])
+      expect(attachment_result[:stream]).to(eq(attachment_raw))
+      expect(attachment_result[:attachment_details][:document_id]).to(eq(product.id))
+      expect(attachment_result[:attachment_details][:content_type]).to(eq("image/gif"))
+      expect(attachment_result[:attachment_details][:name]).to(eq("1x1.gif"))
+      expect(attachment_result[:attachment_details][:size]).to(eq(attachment_raw.size))
     end
   end
 
-  def test_should_delete_attachment
+  it "deletes attachment" do
     store.open_session do |session|
       product = Product.new(nil, "Test Product", 10, "a")
 
@@ -66,10 +66,10 @@ describe RavenDB::AttachmentOperation do
       store.operations.send(
         RavenDB::DeleteAttachmentOperation.new(product.id, "1x1.gif"))
 
-      assert_raises(RavenDB::DocumentDoesNotExistException) do
+      expect do
         store.operations.send(
           RavenDB::GetAttachmentOperation.new(product.id, "1x1.gif", RavenDB::AttachmentType::Document))
-      end
+      end.to(raise_error(RavenDB::DocumentDoesNotExistException))
     end
   end
 end
