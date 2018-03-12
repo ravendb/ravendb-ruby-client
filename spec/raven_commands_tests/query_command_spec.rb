c96f3a3918fe1f611639f9d7d876aee2b8d114c9
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 13:50:41 +0100

    query_command_test: convert to rspec

--- spec/raven_commands_tests/query_command_spec.rb
+++ spec/raven_commands_tests/query_command_spec.rb
@@ -4,7 +4,7 @@ require "minitest/autorun"
 require "spec_helper"
 
 describe RavenDB::QueryCommand do
-  def setup
+  before do
     @__test = RavenDatabaseIndexesTest.new(nil)
     @__test.setup
 
@@ -21,42 +21,42 @@ describe RavenDB::QueryCommand do
     @_index_query = RavenDB::IndexQuery.new(query, {}, nil, nil, wait_for_non_stale_results: true)
   end
 
-  def teardown
+  after do
     @__test.teardown
   end
 
-  def store
+  let(:store) do
     @__test.store
   end
 
-  def request_executor
+  let(:request_executor) do
     @__test.request_executor
   end
 
-  def test_should_do_query
-    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
+  it "does query" do
+    result = request_executor.execute(described_class.new(@_conventions, @_index_query))
 
-    assert(result["Results"].first.key?("Name"))
-    assert_equal("test", result["Results"].first["Name"])
+    expect(result["Results"].first.key?("Name")).to(eq(true))
+    expect(result["Results"].first["Name"]).to(eq("test"))
   end
 
-  def test_test_should_query_only_metadata
-    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, true, false))
+  it "test should query only metadata" do
+    result = request_executor.execute(described_class.new(@_conventions, @_index_query, true, false))
 
-    refute(result["Results"].first.key?("Name"))
+    expect(result["Results"].first.key?("Name")).to(eq(false))
   end
 
-  def test_should_query_only_documents
-    request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
-    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, false, true))
+  it "queries only documents" do
+    request_executor.execute(described_class.new(@_conventions, @_index_query))
+    result = request_executor.execute(described_class.new(@_conventions, @_index_query, false, true))
 
-    refute(result["Results"].first.key?("@metadata"))
+    expect(result["Results"].first.key?("@metadata")).to(eq(false))
   end
 
-  def test_should_fail_with_no_existing_index
-    assert_raises(RavenDB::RavenException) do
+  it "fails with no existing index" do
+    expect do
       @_index_query = RavenDB::IndexQuery.new("from index 'IndexIsNotExists' WHERE Tag = 'Products'", {}, nil, nil, wait_for_non_stale_results: true)
-      request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
-    end
+      request_executor.execute(described_class.new(@_conventions, @_index_query))
+    end.to(raise_error(RavenDB::RavenException))
   end
 end
