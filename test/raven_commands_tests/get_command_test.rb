7aed598e177d5392c1df8817a61af0b579bfbd79
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 14:05:20 +0100

    get_command_test: prepare for conversion

--- test/raven_commands_tests/get_command_test.rb
+++ test/raven_commands_tests/get_command_test.rb
@@ -3,23 +3,32 @@ require "securerandom"
 require "minitest/autorun"
 require "spec_helper"
 
-class GetCommandTest < RavenDatabaseTest
+describe RavenDB::GetDocumentCommand do
   @_put_command = nil
   @_other_put_command = nil
   @_response = nil
   @_other_response = nil
 
   def setup
-    super()
+    @__test = RavenDatabaseTest.new(nil)
+    @__test.setup
 
     @_put_command = RavenDB::PutDocumentCommand.new("Products/101", "Name" => "test", "@metadata" => {})
     @_other_put_command = RavenDB::PutDocumentCommand.new("Products/10", "Name" => "test", "@metadata" => {})
 
-    @_request_executor.execute(@_put_command)
-    @_response = @_request_executor.execute(RavenDB::GetDocumentCommand.new("Products/101"))
+    request_executor.execute(@_put_command)
+    @_response = request_executor.execute(RavenDB::GetDocumentCommand.new("Products/101"))
 
-    @_request_executor.execute(@_other_put_command)
-    @_other_response = @_request_executor.execute(RavenDB::GetDocumentCommand.new("Products/10"))
+    request_executor.execute(@_other_put_command)
+    @_other_response = request_executor.execute(RavenDB::GetDocumentCommand.new("Products/10"))
+  end
+
+  def teardown
+    @__test.teardown
+  end
+
+  def request_executor
+    @__test.request_executor
   end
 
   def test_document_id_should_be_equal_after_load
@@ -31,7 +40,7 @@ class GetCommandTest < RavenDatabaseTest
   end
 
   def test_unexisting_document_loading_attempt_should_return_empty_response
-    result = @_request_executor.execute(RavenDB::GetDocumentCommand.new("product"))
+    result = request_executor.execute(RavenDB::GetDocumentCommand.new("product"))
     assert_nil(result)
   end
 end
