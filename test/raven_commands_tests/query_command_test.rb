78b7b12fdb253a4cf106cbdb2b97df889789ebaf
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 13:47:36 +0100

    query_command_test: prepare for conversion

--- test/raven_commands_tests/query_command_test.rb
+++ test/raven_commands_tests/query_command_test.rb
@@ -3,39 +3,52 @@ require "securerandom"
 require "minitest/autorun"
 require "spec_helper"
 
-class QueryCommandTest < RavenDatabaseIndexesTest
+describe RavenDB::QueryCommand do
   def setup
-    super()
+    @__test = RavenDatabaseIndexesTest.new(nil)
+    @__test.setup
 
     query = "from index 'Testing' where Tag = 'Products'"
 
-    @_request_executor.execute(RavenDB::PutDocumentCommand.new("Products/10",
-                                                               "Name" => "test",
-                                                               "@metadata" => {
-                                                                 "Raven-Ruby-Type": "Product",
-                                                                 "@collection": "Products"
-                                                               }))
+    request_executor.execute(RavenDB::PutDocumentCommand.new("Products/10",
+                                                             "Name" => "test",
+                                                             "@metadata" => {
+                                                               "Raven-Ruby-Type": "Product",
+                                                               "@collection": "Products"
+                                                             }))
 
-    @_conventions = @_store.conventions
+    @_conventions = store.conventions
     @_index_query = RavenDB::IndexQuery.new(query, {}, nil, nil, wait_for_non_stale_results: true)
   end
 
+  def teardown
+    @__test.teardown
+  end
+
+  def store
+    @__test.store
+  end
+
+  def request_executor
+    @__test.request_executor
+  end
+
   def test_should_do_query
-    result = @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
+    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
 
     assert(result["Results"].first.key?("Name"))
     assert_equal("test", result["Results"].first["Name"])
   end
 
   def test_test_should_query_only_metadata
-    result = @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, true, false))
+    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, true, false))
 
     refute(result["Results"].first.key?("Name"))
   end
 
   def test_should_query_only_documents
-    @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
-    result = @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, false, true))
+    request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
+    result = request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query, false, true))
 
     refute(result["Results"].first.key?("@metadata"))
   end
@@ -43,7 +56,7 @@ class QueryCommandTest < RavenDatabaseIndexesTest
   def test_should_fail_with_no_existing_index
     assert_raises(RavenDB::RavenException) do
       @_index_query = RavenDB::IndexQuery.new("from index 'IndexIsNotExists' WHERE Tag = 'Products'", {}, nil, nil, wait_for_non_stale_results: true)
-      @_request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
+      request_executor.execute(RavenDB::QueryCommand.new(@_conventions, @_index_query))
     end
   end
 end
