0e5c68035a9c40c9c713f97a4e79c1fc5a624577
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 13:13:13 +0100

    query_operations_test: prepare for conversion

--- test/raven_commands_tests/query_operations_test.rb
+++ test/raven_commands_tests/query_operations_test.rb
@@ -3,9 +3,10 @@ require "securerandom"
 require "minitest/autorun"
 require "spec_helper"
 
-class QueryOperationsTest < RavenDatabaseTest
+describe RavenDB::QueryCommand do
   def setup
-    super()
+    @__test = RavenDatabaseTest.new(nil)
+    @__test.setup
 
     index_map =
       "from doc in docs.Testings "\
@@ -14,15 +15,27 @@ class QueryOperationsTest < RavenDatabaseTest
       "DocNumber = doc.DocNumber} "
 
     index_sort = RavenDB::IndexDefinition.new("Testing_Sort", index_map)
-    @_store.operations.send(RavenDB::PutIndexesOperation.new(index_sort))
+    store.operations.send(RavenDB::PutIndexesOperation.new(index_sort))
 
     (0..99).each do |i|
-      @_request_executor.execute(RavenDB::PutDocumentCommand.new("Testings/#{i}",
-                                                                 "Name" => "test#{i}", "DocNumber" => i,
-                                                                 "@metadata": {"@collection" => "Testings"}))
+      request_executor.execute(RavenDB::PutDocumentCommand.new("Testings/#{i}",
+                                                               "Name" => "test#{i}", "DocNumber" => i,
+                                                               "@metadata": {"@collection" => "Testings"}))
     end
 
-    @_request_executor.execute(RavenDB::QueryCommand.new(@_store.conventions, RavenDB::IndexQuery.new("from index 'Testing_Sort'", {}, nil, nil, wait_for_non_stale_results: true)))
+    request_executor.execute(RavenDB::QueryCommand.new(store.conventions, RavenDB::IndexQuery.new("from index 'Testing_Sort'", {}, nil, nil, wait_for_non_stale_results: true)))
+  end
+
+  def teardown
+    @__test.teardown
+  end
+
+  def store
+    @__test.store
+  end
+
+  def request_executor
+    @__test.request_executor
   end
 
   def test_update_by_index_success
@@ -30,7 +43,7 @@ class QueryOperationsTest < RavenDatabaseTest
     index_query = RavenDB::IndexQuery.new(query, {name: "Patched"}, nil, nil, wait_for_non_stale_results: true)
     patch_by_index_operation = RavenDB::PatchByQueryOperation.new(index_query, RavenDB::QueryOperationOptions.new(false))
 
-    response = @_store.operations.send(patch_by_index_operation)
+    response = store.operations.send(patch_by_index_operation)
 
     refute_nil(response)
     refute(response["Result"]["Total"] < 100)
@@ -38,7 +51,7 @@ class QueryOperationsTest < RavenDatabaseTest
     query = "from index 'Testing_Sort' where Name = $name"
     index_query = RavenDB::IndexQuery.new(query, {name: "Patched"}, nil, nil, wait_for_non_stale_results: true)
 
-    response = @_request_executor.execute(RavenDB::QueryCommand.new(@_store.conventions, index_query))
+    response = request_executor.execute(RavenDB::QueryCommand.new(store.conventions, index_query))
     assert(response.key?("Results"))
     assert(response["Results"].is_a?(Array))
     refute(response["Results"].length < 100)
@@ -50,7 +63,7 @@ class QueryOperationsTest < RavenDatabaseTest
     patch_by_index_operation = RavenDB::PatchByQueryOperation.new(index_query, RavenDB::QueryOperationOptions.new(false))
 
     assert_raises(RavenDB::IndexDoesNotExistException) do
-      @_store.operations.send(patch_by_index_operation)
+      store.operations.send(patch_by_index_operation)
     end
   end
 
@@ -59,11 +72,11 @@ class QueryOperationsTest < RavenDatabaseTest
     index_query = RavenDB::IndexQuery.new(query, {min: 0, max: 49}, nil, nil, wait_for_non_stale_results: true)
     delete_by_index_operation = RavenDB::DeleteByQueryOperation.new(index_query, RavenDB::QueryOperationOptions.new(false))
 
-    response = @_store.operations.send(delete_by_index_operation)
+    response = store.operations.send(delete_by_index_operation)
     assert_equal("Completed", response["Status"])
 
-    query_command = RavenDB::QueryCommand.new(@_store.conventions, index_query)
-    response = @_request_executor.execute(query_command)
+    query_command = RavenDB::QueryCommand.new(store.conventions, index_query)
+    response = request_executor.execute(query_command)
     assert(response.key?("Results"))
     assert(response["Results"].is_a?(Array))
     assert_equal(0, response["Results"].length)
@@ -75,7 +88,7 @@ class QueryOperationsTest < RavenDatabaseTest
     delete_by_index_operation = RavenDB::DeleteByQueryOperation.new(index_query, RavenDB::QueryOperationOptions.new(false))
 
     assert_raises(RavenDB::IndexDoesNotExistException) do
-      @_store.operations.send(delete_by_index_operation)
+      store.operations.send(delete_by_index_operation)
     end
   end
 end
