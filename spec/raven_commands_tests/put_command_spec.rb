cf739251c99505bf7becb5098a5d897204311935
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 14:37:20 +0100

    put_command_test: convert to rspec

---
+++ spec/raven_commands_tests/put_command_spec.rb
@@ -0,0 +1,31 @@
+require "ravendb"
+require "securerandom"
+require "minitest/autorun"
+require "spec_helper"
+
+describe RavenDB::PutDocumentCommand do
+  before do
+    @__test = RavenDatabaseTest.new(nil)
+    @__test.setup
+  end
+
+  after do
+    @__test.teardown
+  end
+
+  def request_executor
+    @__test.request_executor
+  end
+
+  it "puts successfully" do
+    request_executor.execute(described_class.new("Testings/1", "name" => "test", "@metadata" => {:@id => "Testings/1", "@collection" => "testings"}))
+    result = request_executor.execute(RavenDB::GetDocumentCommand.new("Testings/1"))
+    expect(result["Results"].first["@metadata"]["@id"]).to(eq("Testings/1"))
+  end
+
+  it "fails with invalid json" do
+    expect do
+      request_executor.execute(described_class.new("testing/2", "invalid json"))
+    end.to(raise_error(RavenDB::RavenException))
+  end
+end
