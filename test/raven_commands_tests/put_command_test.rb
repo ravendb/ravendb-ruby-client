6122e7e115ed5e410ed2cbb50f57dd1afb23c133
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-03-12 14:18:30 +0100

    put_command_test: prepare for conversion

--- test/raven_commands_tests/put_command_test.rb
+++ test/raven_commands_tests/put_command_test.rb
@@ -3,16 +3,29 @@ require "securerandom"
 require "minitest/autorun"
 require "spec_helper"
 
-class PutCommandTest < RavenDatabaseTest
+describe RavenDB::PutDocumentCommand do
+  def setup
+    @__test = RavenDatabaseTest.new(nil)
+    @__test.setup
+  end
+
+  def teardown
+    @__test.teardown
+  end
+
+  def request_executor
+    @__test.request_executor
+  end
+
   def test_should_put_successfully
-    @_request_executor.execute(RavenDB::PutDocumentCommand.new("Testings/1", "name" => "test", "@metadata" => {"@id": "Testings/1", "@collection" => "testings"}))
-    result = @_request_executor.execute(RavenDB::GetDocumentCommand.new("Testings/1"))
+    request_executor.execute(RavenDB::PutDocumentCommand.new("Testings/1", "name" => "test", "@metadata" => {"@id": "Testings/1", "@collection" => "testings"}))
+    result = request_executor.execute(RavenDB::GetDocumentCommand.new("Testings/1"))
     assert_equal("Testings/1", result["Results"].first["@metadata"]["@id"])
   end
 
   def test_should_fail_with_invalid_json
     assert_raises(RavenDB::RavenException) do
-      @_request_executor.execute(RavenDB::PutDocumentCommand.new("testing/2", "invalid json"))
+      request_executor.execute(RavenDB::PutDocumentCommand.new("testing/2", "invalid json"))
     end
   end
 end
